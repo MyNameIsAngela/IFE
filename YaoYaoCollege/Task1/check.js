53591d576a46593f115c5e674de2edc2f6cd4415
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-06-02 16:13:08 +0800

    完成输入为空的判断
    1.引入外部css文件
    2.判断输入是否为空

--- YaoYaoCollege/Task1/check.js
+++ YaoYaoCollege/Task1/check.js
@@ -1,9 +1,22 @@
 
-function check(){
-	var setLen;
-	var notNul;
-	var corFor;
+
+function varify(){	
+	var check = document.getElementById("check");
+	var message = document.getElementById("message");
 	
-	var setLen = document.getElementById("settingLength").value;
-	console.log(setLen);
+	// 判断输入内容是否为空
+	if(check.value ==""){
+		check.style.border="2px solid red";
+		check.style.color="red";
+		message.innerHTML="姓名不能为空";
+	}else{
+		test(check.value);
+	};
+
+	// 判断输入长度是否正确
+
+
+	// console.log(check);
+	// document.getElementById("con").innerHTML="hello world11";
+	// document.write("必填，长度为4～16个字符");
 }
