6f7c7192259f4bb1081b03d2896ce42580fa251e
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-08-30 16:38:42 +0800

    tetris俄罗斯方块【完成10%】 黑色长条下落
    已完成：
    方块可随时间间隔下落、
    按键可操控方块、
    初始画方块的方法。
    
    待解决：
    绘制其他六种方块画法、
    目前绘制方法不能构成体系，要使用数组存储、
    按up键实现顺时针旋转。

--- tetris/js/main.js
+++ tetris/js/main.js
@@ -11,6 +11,12 @@ var deltaTime;//两帧间隔时间差
 var bgPic = new Image();//存储背景图片
 
 var shape;//俄罗斯方块的各种形状
+var unit = 30;
+
+var mx=0;//定义按键后坐标偏移的单位距离
+var my=0;
+var keyboard;//定义按键的类型
+var speed=1000;//定义下降速度，初始值为1000，即下降时间间隔为1秒
 
 document.body.onload = game;
 function game(){
@@ -19,6 +25,7 @@ function game(){
 	lastTime = Date.now();
 	deltaTime = 0;
 	gameloop();
+
 }
 
 function init(){
@@ -27,22 +34,56 @@ function init(){
 	can2 = document.getElementById('canvas2');
 	ctx2 = can2.getContext('2d');
 
+	window.addEventListener('keydown', onKeydown, false); //捕捉鼠标的动作
+	//window.addEventListener('keypress', onKeydown, true); //捕捉鼠标的动作
+	//window.addEventListener('keyup', onKeydown, true); //捕捉鼠标的动作
+
 	bgPic.src = "background.jpeg";
 	canWidth = can1.width;
 	canHeigh = can1.height;
 
 	shape = new shapeObj();//画下落的七种形状
 	shape.init();
+
+	mx = 0;
+	my = 0;
 }
 
 function gameloop(){
 	window.requestAnimFrame(gameloop);
 	// console.log("loop");
 	var now = Date.now();
-	deltaTime = now - lastTime;
+	deltaTime = deltaTime + now - lastTime;
 	lastTime = now;
 
 	// console.log(deltaTime);
-	drawBackground();
+	//drawBackground();
+	if(deltaTime>1000){
+		deltaTime=0;
+		my = my+unit;
+	}
+	ctx1.clearRect(0,0,canWidth,canHeigh);//将前一帧的内容清空，绘制新的
+	shape.draw(mx,my);
 
 }
+
+function onKeydown(e){
+	if(event.keyCode==37 || event.keyCode==65){//left
+		mx = mx-unit;
+		console.log("onKeydown"+mx,my);
+	}
+	if(event.keyCode==39 || event.keyCode==68){//right
+		mx = mx+unit;
+		console.log(mx,my);
+	}
+	if(event.keyCode==38 || event.keyCode==87){//up
+		my = my-unit;
+		console.log(mx,my);
+	}
+	if(event.keyCode==40 || event.keyCode==83){//down
+		my = my+unit;
+		console.log(mx,my);
+	}
+}
+
+
--- tetris/js/shape.js
+++ tetris/js/shape.js
@@ -1,21 +1,39 @@
-var shapeObj = function(){ //定义海葵anemone 对象的类
-this.centerX;
-this.centerY;
-this.leftX;
-this.leftY;
-this.rightX;
-this.rightY;
+var shapeObj = function(){ //定义7种形状 shape 对象的类
+	this.centerX;
+	this.centerY;
+	this.leftX;
+	this.leftY;
+	this.rightX;
+	this.rightY;
+	this.width;
+	this.heigh;
 }
-shapeObj.prototype.init = function(){//初始化，确定每一个海葵的位置  此处从底部画上去
-	centerX = canWidth/2;
-	centerY = 0;
+shapeObj.prototype.init = function(){//初始化，画出每个形状
+	this.centerX = mx+canWidth/2;
+	this.centerY = my;
 
-	for(var i = 0; i < this.num; i++){
-		this.x[i] = i * 10 + Math.random() * 20; //Math.random()返回 [0,1）之间的值
-		this.len[i] = 200 + Math.random * 50;
-	}
-	console.log("ane");
+	this.leftX = this.centerX-2*unit;
+	this.leftY = this.centerY;
+	//this.rightX = this.centerX+2*unit;
+	//this.rightY = this.centerY;
+	this.width=4*unit;
+	this.heigh=1*unit;
 }
 shapeObj.prototype.draw = function(){ //绘制到canvas上
+	this.centerX = mx+canWidth/2;
+	this.centerY = my;
+	this.leftX = this.centerX-2*unit;
+	this.leftY = this.centerY;
+	//console.log(this.centerX,this.centerY);
 
+	ctx1.save();
+	//beginPath,moveTo,lineTo,stroke,strokeStyle,lineWidth,lineCap,globalAlpha
+	ctx1.beginPath();
+	//ctx1.arc(200,200,100,0,Math.PI*2,true);//画圆的例子
+	ctx1.fillRect(this.leftX, this.leftY,this.width,this.heigh);
+	//ctx1.moveTo(this.leftX, this.leftY);
+	//ctx1.lineTo(this.rightX, this.rightY);
+	ctx1.fillStyle="#0000ff";//未着手成功
+	ctx1.stroke();
+	ctx1.restore();
 }
